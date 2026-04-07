9af048376b912e72cc911f322a11140c02a29d7b
Prulucas <plbrandsilva@gmail.com>
2026-04-07 14:34:53 -0300

    criando blocos de conteudo (plus de criar página de erro 400)

--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+'''
+class NotFoundView(TemplateView):
+    template_name = '404.html'
+'''
